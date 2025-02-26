3bbcfbd9c48aedc88e7645c537de4d384ab61921
codingFer <codingFer@gmail.com>
2025-02-26 00:39:02 -0400

    Initial commit

---
+++ projects/calendar/src/lib/calendar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cfer-calendar',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      calendar works! :v2
+    </p>
+  `,
+  styles: ``
+})
+export class CalendarComponent {
+
+}
---
+++ projects/calendar/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of calendar
+ */
+
+export * from './lib/calendar.service';
+export * from './lib/calendar.component';
